ea502ce4f0ff00dcd88b652b3f4f7d117c32d278
unknown <anil.yadav2126@outlook.com>
2025-06-30 18:50:57 +0530

    Life insurance Project V2

--- Infra/Dev/main.tf
+++ Infra/Dev/main.tf
@@ -1,4 +1,7 @@
+
 # Main module to call the child modules
+
+# Resource Group module: Creates an Azure Resource Group using the specified name and location
 module "resource_group" {
   source = "../../modules/azurerm_resource_group"
 
@@ -6,13 +9,15 @@ module "resource_group" {
   location = var.location_name
 
 }
+
+# Storage Account module: Creates an Azure Storage Account in the above Resource Group
 module "storage_account" {
   source = "../../modules/azurerm_storage_account"
 
-  depends_on = [module.resource_group]
+  depends_on = [module.resource_group] # Ensures the resource group is created first
 
-  rg_name  = var.name_rg
-  location = var.location_name
+  rg_name     = var.name_rg
+  location    = var.location_name
   stgacc_name = var.name_stgacc
-
+  
 }
