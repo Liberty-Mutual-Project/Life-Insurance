7e21c0e18ee398885f4dbd0c6bc88c86ff9f8264
unknown <anil.yadav2126@outlook.com>
2025-06-30 18:53:51 +0530

    Life insurance Project V3

--- modules/azurerm_resource_group/azurerm_resource_group.tf
+++ modules/azurerm_resource_group/azurerm_resource_group.tf
@@ -1,3 +1,5 @@
+# This Terraform module creates an Azure Resource Group using the provided name and location variables.
+
 resource "azurerm_resource_group" "pagal123" {
   name     = var.rg_name
   location = var.location
--- modules/azurerm_storage_account/azurerm_storage_account.tf
+++ modules/azurerm_storage_account/azurerm_storage_account.tf
@@ -1,3 +1,7 @@
+# यह संसाधन एक वर्चुअल मशीन बनाता है।
+# इसमें वर्चुअल मशीन का नाम, लोकेशन, और आवश्यक कॉन्फ़िगरेशन शामिल हैं।
+# कृपया सुनिश्चित करें कि सभी आवश्यक इनपुट मान सही ढंग से प्रदान किए गए हैं।
+
 resource "azurerm_storage_account" "pagal1storage" {
   name = var.stgacc_name
   resource_group_name      = var.rg_name
