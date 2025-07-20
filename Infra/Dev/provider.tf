8f2be4ffbe0010223e3458ab1fa7ec409d44b6ff
Anil-Yadav2126 <anilyadav_cse@yahoo.com>
2025-07-20 20:22:59 +0530

    provider.tf

--- Infra/Dev/provider.tf
+++ Infra/Dev/provider.tf
@@ -7,15 +7,9 @@ terraform {
       version = "4.23.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "paglait"
-    storage_account_name = "pagalstorageacc"
-    container_name       = "tfstate"
-    key                  = "Dev.terraform.tfstate"
-  }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "8f765261-0137-4fd7-b8de-53247b5236d0"
+  subscription_id = "e8ad8a11-5e89-4545-9309-27ed1a0cd62f"
 
 }
